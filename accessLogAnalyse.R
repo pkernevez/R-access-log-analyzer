0219686c93111e4db0dd9add07958046d5b2b67f
P.Kernevez <pkernevez@octo.com>
2014-11-18 19:08:27 +0100

    V1 qui fonctionne

--- accessLogAnalyse.R
+++ accessLogAnalyse.R
@@ -1,12 +1,17 @@
 # Lecture du fichier CSV 
 
-DATE_FORMAT="[%d/%b/%Y:%H:%M:%S"
+FILE_DATE_FORMAT="[%d/%b/%Y:%H:%M:%S"
+RENDER_DATE_FORMAT="%Y/%m/%d %H:%M:%S"
 #DEFAULT_FILE_NAME="data/access_generic.log"
-DEFAULT_FILE_NAME="data/access_generic_extract.log"
-INTERVAL_IN_SECONDS = 3600 # 3600 !
+#DEFAULT_FILE_NAME="data/access_generic_extract.log"
+DEFAULT_FILE_NAME="data/access_generic_extract_small.log"
+INTERVAL_IN_SECONDS = 30 # 3600 !
+URL_EXTRACT_SIZE=30
 #QQQ Adapter la legende du graph en fonction de la valeur de l'interval
-WIDTH=800
-HEIGHT=600
+S_WIDTH=8
+S_HEIGHT=8
+B_WIDTH=16
+B_HEIGHT=8
 PERCENTILE_FOR_DISTRIBUTION=0.995
 ERROR_PATTERN='(5..|404)'
 print("Start")
@@ -17,16 +22,13 @@ cmd = commandArgs(trailingOnly = TRUE)
   FILE_NAME = DEFAULT_FILE_NAME
 #}
 
-load_and_install = function(lib){
-  if(!lib %in% installed.packages()[,"Package"]) install.packages(lib)
-  suppressPackageStartupMessages(library(lib,character.only=TRUE))
+if (INTERVAL_IN_SECONDS>=3600) {
+  INTERVAL_AS_TEXT = paste(round(INTERVAL_IN_SECONDS/3600,2), "hour(s)")
+} else if (INTERVAL_IN_SECONDS>=60) {
+  INTERVAL_AS_TEXT = paste(round(INTERVAL_IN_SECONDS/60,2), "minute(s)")
+} else {
+  INTERVAL_AS_TEXT = paste(round(INTERVAL_IN_SECONDS,2), "second(s)")
 }
-load_and_install("stringr")
-load_and_install("ggplot2")
-load_and_install("colorspace")
-load_and_install("ascii")
-load_and_install("knitr")
-#library
 
 CATEGORIES=list(
     "PAC"=".*\\.pac HTTP/",
@@ -37,6 +39,21 @@ CATEGORIES=list(
   )
 CATEGORY_NAMES = c(names(CATEGORIES),"Other")
 
+extract = function(url){
+  urlChar = as.character(url)
+  if (nchar(urlChar)>URL_EXTRACT_SIZE) {
+    return(paste0(substr(urlChar, 1, URL_EXTRACT_SIZE), "..."))
+  } else {
+    return(urlChar)
+  }
+}
+
+cleanStr = function(str){
+  str = gsub("\\\\", "&#92;", str)
+  str = gsub("\\|", "&#124;", str)
+  return(str)
+}
+
 ReadLogFile <- function(file ) {
   # http://en.wikipedia.org/wiki/Common_Log_Format
   print("Loading file")
@@ -44,7 +61,7 @@ ReadLogFile <- function(file ) {
                                                "time_zone", "request", "status", "response.size", "response.time_microsec"))
   
   
-  access_log$ts <- strptime(access_log$ts, format = DATE_FORMAT)
+  access_log$ts <- strptime(access_log$ts, format = FILE_DATE_FORMAT)
   access_log$time_zone <- as.factor(sub("\\]", "", access_log$time_zone))
   access_log$status <- as.factor(sub("\\]", "", access_log$status))
   print("Create category")
@@ -58,32 +75,13 @@ ReadLogFile <- function(file ) {
     access_log[ with( access_log, grepl(pattern, request)), "category"] = name
   }
   access_log$category = factor(access_log$category, levels=CATEGORY_NAMES)
+  access_log$url_extract = sapply(access_log$request, extract)
   print("File loaded")
   access_log
 }
 if (!file.exists("out")) dir.create("out")
 access_log <- ReadLogFile(FILE_NAME)
 head(access_log, 50)
-
-png("out/all_request_per_hours.png", width = WIDTH, height=HEIGHT)
-  print("Creating all_request_per_hours.png")
-  g = ggplot(access_log, aes(x = ts)) + 
-  #   geom_density( group="category",
-  geom_density(stat = "bin", binwidth = INTERVAL_IN_SECONDS,
-          colour = "black", fill = "darkgreen") + ylab("Requests/hour") + xlab("Time") 
-#          stat_smooth(data=dataJourOuvre, aes(x= date, y=actions_dossiers), colour="red",method = "gam", formula = y ~ s(x, bs = "cs"))+
-#scale_color_manual(values = c("actions_dossiers"="blue"))
-  print(g)
-dev.off()
-
-png("out/all_request_per_hours_by_type.png", width = WIDTH, height=HEIGHT)
-print("Creating all_request_per_hours_by_type.png")
-#ggplot(access_log, aes(x = ts)) + geom_density(aes(fill=category,order=-as.numeric(category)), position="stack", size=2)
-g = ggplot(access_log, aes(x = ts)) + 
-  geom_density(stat = "bin", binwidth = INTERVAL_IN_SECONDS, position="stack", aes(fill = category, color=category, order=-as.numeric(category))) + 
-     ylab("Requests/hour") + xlab("Time") 
-print(g)
-dev.off()
 
 #DISTRIBUTION
 png("out/all_responsetime_distribution.png", width = WIDTH, height=HEIGHT)
@@ -119,48 +117,6 @@ for (cat in CATEGORY_NAMES) {
   dev.off()
 }
 
-#Code retour
-png("out/response_code.png", width = WIDTH, height=HEIGHT)
-  print("Creating response_code.png")
-  df <- as.data.frame(table(access_log$status))
-  colnames(df) <- c('status','freq')
-  g = ggplot(df, aes(x = "", y = freq, fill = status, color = status)) +
-    geom_bar(width = 1, stat = "identity") +
-    coord_polar("y", start = 0) +
-    labs(title = "Répartition des codes retour HTTP") + xlab("")
-  print(g)
-dev.off()
-
-#CODE D'erreur 5xx et 404 Camemberg
-png("out/response_error.png", width = WIDTH, height=HEIGHT)
-print("Creating response_error.png")
-server.errors <- grep(ERROR_PATTERN,access_log$status)
-g = ggplot(access_log[server.errors,], aes(x=status)) + geom_histogram(colour="black", fill="red") +
-  labs(title = "Répartition des codes d'erreur HTTP")
-print(g)
-dev.off()
-
-#HTTP Method Camemberg
-png("out/http_method.png", width = WIDTH, height=HEIGHT)
-print("Creating http_method.png")
-df <- as.data.frame(table(access_log$method))
-colnames(df) <- c('method','freq')
-g = ggplot(df, aes(x = "", y = freq, fill = method, color = method)) +
-  geom_bar(width = 1, stat = "identity") +
-  coord_polar("y", start = 0) +
-  labs(title = "HTTP Methods distribution") + xlab("")
-  print(g)
-dev.off()
-
-#Code d'erreur en fonction du temps
-png("out/response_error_by_time.png", width = WIDTH, height=HEIGHT)
-print("Creating response_error_by_time.png")
-g = ggplot(access_log[server.errors,], aes(x=ts)) +
-  geom_density(stat='bin',binwidth=3600, position="stack") +
-  aes(fill = status, color=status, order=-as.numeric(category)) +
-  ylab('Errors/hour') + xlab('Time')
-  print(g)
-dev.off()
 
 #Temps de reponse global et un fichier par type : timeseries + smooth
 png("out/response_time_by_time.png", width = WIDTH, height=HEIGHT)
--- accessLogAnalyzer.R
+++ accessLogAnalyzer.R
@@ -5,11 +5,20 @@ load_and_install = function(lib){
 load_and_install("stringr")
 load_and_install("ggplot2")
 load_and_install("colorspace")
-load_and_install("ascii")
 load_and_install("knitr")
-load_and_install("rmarkdown")
+load_and_install("markdown")
+load_and_install("pander")
 
-source("src/accessLogAnalyzer.R")
+source("accessLogAnalyse.R")
+log <- function(data, ..., sep=""){
+  write(paste(data, ..., sep=sep), stderr())
+}
 
-rmarkdown::render("analyseTemplate.Rmd")
-markdownToHTML
+opts_knit$set(aliases=c(h = 'fig.height', w = 'fig.width'))
+knit("analyseTemplate.Rmd")
+markdownToHTML("analyseTemplate.md","analyse.html", title="Access log analysis", stylesheet="analyse.css",  options=c(markdownHTMLOptions(defaults = TRUE), "toc"))
+#rmarkdown::render("analyseTemplate.md")
+#markdownExtensions()
+#cat(markdownToHTML(text = "## Next Steps {:id ID}", options=c("")))
+
+
